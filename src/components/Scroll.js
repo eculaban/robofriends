c5e9dc960357596b49b532ed06fcc27d41dc7703
Elizalde Culaban <culabanelizalde@gmail.com>
2021-04-05 17:03:47 +0800

    initial commit

---
+++ src/components/Scroll.js
@@ -0,0 +1,9 @@
+const Scroll = (props) => {
+	return (
+		<div style={{overflowY: 'scroll', height: '500px'}}>
+			{props.children}
+		</div>
+	);
+}
+
+export default Scroll
---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Sean Michael',
+		username: 'El',
+		email: 'elcul@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Ging Ging',
+		username: 'Eli',
+		email: 'elicul@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'El',
+		username: 'Elizalde',
+		email: 'elizaldeculaban@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Kirvy',
+		username: 'Jb',
+		email: 'jb@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Asasa',
+		username: 'Asass',
+		email: 'asass@gmail.com'
+	},
+	{
+		id: 6,
+		name: 'Test',
+		username: 'test_user',
+		email: 'testuser@gmail.com'
+	}
+]
